b24f9694e9b3455add65f32ce8485c0bd5f62f4d
HossinAmin <hossinamin@diverse.dev>
2023-09-27 01:06:49 +0300

    filter drop down

--- src/components/FilterDropdown.tsx
+++ src/components/FilterDropdown.tsx
@@ -2,11 +2,22 @@ import { useState } from "react";
 import { Icon } from "@iconify/react";
 
 interface DropdownProps {
-  options: string[];
   onSelect: (option: string) => void;
 }
 
-export default function FilterDropdown({ options, onSelect }: DropdownProps) {
+const options = [
+  "Applied",
+  "Shortlisted",
+  "Technical Interview",
+  "Opportunity Browsing",
+  "Video Interview I",
+  "Video Interview II",
+  "Video Interview III",
+  "Offer",
+  "Withdrawn",
+];
+
+export default function FilterDropdown({ onSelect }: DropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
@@ -21,29 +32,33 @@ export default function FilterDropdown({ options, onSelect }: DropdownProps) {
   };
 
   return (
-    <div className="relative w-[15rem]">
+    <div className="relative w-[18rem]">
       <div
-        className={`flex cursor-pointer items-center justify-between rounded-3xl border px-2
-        ${isOpen ? "bg-primary/10" : "bg-white"}`}
+        className={`mb-1 flex cursor-pointer select-none items-center justify-between rounded-3xl border bg-white px-3 py-2
+        ${isOpen ? "rounded-b-none " : ""}`}
         onClick={toggleDropdown}
       >
-        <span className="p-2 text-lg text-primary">
+        <span className="text-primary">
           {selectedOption || "Select an option"}
         </span>
         <Icon
           icon="majesticons:chevron-down-line"
-          className="text-2xl text-primary "
+          className="text-2xl text-primary"
         />
       </div>
       {isOpen && (
-        <ul className="absolute mt-2 w-40 rounded border border-primary bg-white py-2">
+        <ul className="absolute w-full select-none rounded-lg rounded-t-none bg-white  shadow-lg ">
           {options.map((option, index) => (
             <li
               key={index}
-              className="hover:bg-primary-100 cursor-pointer px-4 py-2"
+              className={`flex w-full cursor-pointer items-center justify-between px-3 py-5 last:border-none odd:border-b-2 even:border-b-2  hover:bg-primary/10 active:bg-primary/5
+              ${option == selectedOption ? "bg-primary/10" : ""}`}
               onClick={() => handleOptionSelect(option)}
             >
-              {option}
+              <span>{option}</span>
+              <span className="flex items-center justify-center rounded-full bg-[#F8F8F8] px-4 py-1 text-center text-sm text-[#444444]">
+                {Math.ceil(Math.random() * 100)}
+              </span>
             </li>
           ))}
         </ul>
